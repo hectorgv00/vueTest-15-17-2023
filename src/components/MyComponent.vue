77e5ef15f0c01d284dea4ede11fce8a3ed1a7f34
Hector Gonzalez Viejo <hector.gv00@gmail.com>
2023-07-15 12:26:39 +0200

    first commit

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="myComponent">
+    <!-- v-on: and @ si the same -->
+    <button @click=" $event => toggleVisibility()" >AParece o desaparece</button>
+    <h1 v-if="isVisible"> {{ msg }} </h1>
+    <input type="text" v-model="msg">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'myComponent',
+  data(){
+    return{
+      msg: 'Welcome to Your Vue.js App',
+      isVisible: false
+    }
+  },
+  methods:{
+    toggleVisibility(){
+      this.isVisible = !this.isVisible
+    }
+  }
+}
+
+</script>
